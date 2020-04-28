b23e4b91fa85d4c75f3975cdb534d0f00d0f507c
GaneshSansoa <gchaudhary1995@gmail.com>
2020-04-28 06:50:19 +0530

    first commit

---
+++ login.php
@@ -0,0 +1,199 @@
+<?php 
+include_once('config/config.php');
+include(ROOT_PATH ."header.php");?>
+        <!-- ============================================================== -->
+        <!-- Page wrapper  -->
+        <!-- ============================================================== -->
+        <div class="page-wrapper">
+            <!-- ============================================================== -->
+            <!-- Container fluid  -->
+            <!-- ============================================================== -->
+            <div class="container-fluid">
+                <!-- ============================================================== -->
+                <!-- Static Slider 1  -->
+                <!-- ============================================================== -->
+                <div class="bg-light">
+                    <section>
+                        <div id="banner2" class="banner spacer" style="background-image:url(images/background-4.svg);">
+                            <div class="container">
+                                <div class="row justify-content-center">
+                                    <div class="col-md-7 col-lg-5 aos-init aos-animate" data-aos="fade-up" data-aos-duration="1500">
+                                       
+                                        <div class="m-t-40">
+                                          <div class="card card-login">
+											<form class="form" id="login_form" method="post" action="" onsubmit="return false;">
+												<div class="header header-primary text-center">
+													<h4 class="card-title">LOG IN</h4>
+													<div class="social-line">
+														<a href="#pablo" class="btn btn-just-icon btn-simple">
+															<i class="fa fa-facebook-square"></i>
+														</a>
+														<a href="#pablo" class="btn btn-just-icon btn-simple">
+															<i class="fa fa-twitter"></i>
+														</a>
+														<a href="#pablo" class="btn btn-just-icon btn-simple">
+															<i class="fa fa-google-plus"></i>
+														</a>
+													</div>
+												</div>
+												
+												<div class="card-content">
+
+
+													<div class="input-group">
+														<label class="sr-only" for="inlineFormInputGroup">Email</label>
+													  <div class="input-group mb-2">
+														<div class="input-group-prepend">
+														  <div class="input-group-text">
+															<i class="material-icons">email</i>
+														
+														  </div>
+														</div>
+														<input type="text" class="form-control" name="email" id="login_email" placeholder="Email" data-toggle="popover" data-html="true" data-trigger="manual" data-content="lola" data-placement="right">
+													</div>
+													</div>
+													<div class="input-group">
+														<label class="sr-only" for="inlineFormInputGroup">Password</label>
+													  <div class="input-group mb-2">
+														<div class="input-group-prepend">
+														  <div class="input-group-text">
+															<i class="material-icons">lock_outline</i>
+														
+														  </div>
+														</div>
+                                                        <input type="password" class="form-control" name="password" id="login_password" placeholder="Password" data-toggle="popover" data-html="true" data-trigger="manual" data-placement="right">
+                                                        
+                                                    </div>
+
+													</div>
+                                                    <div class="form-control-feedback text-center hide">Success! You've done it.</div>
+													<!-- If you want to add a checkbox to this form, uncomment this code
+
+													<div class="checkbox">
+														<label>
+															<input type="checkbox" name="optionsCheckboxes" checked>
+															Subscribe to newsletter
+														</label>
+													</div> -->
+												</div>
+												<div class="footer text-center">
+													<button type="submit" id="form-submit" class="btn btn-primary btn-simple btn-wd btn-blue-gradiant">Get Started</button>
+												</div>
+											</form>
+					</div>
+											</div> 
+                                            
+                                           
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+					<section>
+						<div class="container-fluid">
+							<div class="row">
+								<div class="col-sm-6">
+									
+								</div>
+						</div>
+					</section>
+					<section>
+						<div class="bg-light spacer feature5">
+							<div class="container">
+								<!-- Row  -->
+								<div class="row justify-content-center">
+									<div class="col-md-7 text-center">
+										<h2 class="title">Create Your Own Citation</h2>
+										<h6 class="subtitle">By Following Below Steps</h6>
+									</div>
+								</div>
+								<!-- Row  -->
+								<div class="row m-t-40">
+									<!-- Column -->
+									<div class="col-md-4 wrap-feature5-box">
+										<div class="card card-shadow" data-aos="fade-right" data-aos-duration="1200">
+											<div class="card-body d-flex">
+												<div class="icon-space"><i class="text-danger-gradiant icon-Stopwatch"></i></div>
+												<div class="">
+													<h6 class="font-medium"><a href="javascript:void(0)" class="linking">Choose Type</a></h6>
+													<p class="m-t-20">You can relay on our amazing features list and also our customer services.</p>
+												</div>
+											</div>
+										</div>
+									</div>
+									<!-- Column -->
+									<!-- Column -->
+									<div class="col-md-4 wrap-feature5-box">
+										<div class="card card-shadow" data-aos="fade-down" data-aos-duration="1200">
+											<div class="card-body d-flex">
+												<div class="icon-space"><i class="text-danger-gradiant icon-Information"></i></div>
+												<div class="">
+													<h6 class="font-medium"><a href="javascript:void(0)" class="linking">Powerful Tech </a></h6>
+													<p class="m-t-20">You can relay on our amazing features list and also our customer services.</p>
+												</div>
+											</div>
+										</div>
+									</div>
+									<!-- Column -->
+									<!-- Column -->
+									<div class="col-md-4 wrap-feature5-box">
+										<div class="card card-shadow" data-aos="fade-left" data-aos-duration="1200">
+											<div class="card-body d-flex">
+												<div class="icon-space"><i class="text-danger-gradiant icon-Leo-2"></i></div>
+												<div class="">
+													<h6 class="font-medium"><a href="javascript:void(0)" class="linking">100% Satisfaction </a></h6>
+													<p class="m-t-20">You can relay on our amazing features list and also our customer services.</p>
+												</div>
+											</div>
+										</div>
+									</div>
+									<!-- Column -->
+									<!-- Column -->
+									<div class="col-md-4 wrap-feature5-box">
+										<div class="card card-shadow" data-aos="fade-right" data-aos-duration="1200">
+											<div class="card-body d-flex">
+												<div class="icon-space"><i class="text-danger-gradiant icon-Target-Market"></i></div>
+												<div class="">
+													<h6 class="font-medium"><a href="javascript:void(0)" class="linking">Targeting Market</a></h6>
+													<p class="m-t-20">You can relay on our amazing features list and also our customer services.</p>
+												</div>
+											</div>
+										</div>
+									</div>
+									<!-- Column -->
+									<!-- Column -->
+									<div class="col-md-4 wrap-feature5-box">
+										<div class="card card-shadow" data-aos="fade-up" data-aos-duration="1200">
+											<div class="card-body d-flex">
+												<div class="icon-space"><i class="text-danger-gradiant icon-Sunglasses-Smiley"></i></div>
+												<div class="">
+													<h6 class="font-medium"><a href="javascript:void(0)" class="linking">Goal Achievement </a></h6>
+													<p class="m-t-20">You can relay on our amazing features list and also our customer services.</p>
+												</div>
+											</div>
+										</div>
+									</div>
+									<!-- Column -->
+									<!-- Column -->
+									<div class="col-md-4 wrap-feature5-box">
+										<div class="card card-shadow" data-aos="fade-left" data-aos-duration="1200">
+											<div class="card-body d-flex">
+												<div class="icon-space"><i class="text-danger-gradiant  icon-Laptop-Phone"></i></div>
+												<div class="">
+													<h6 class="font-medium"><a href="javascript:void(0)" class="linking">Fully Responsive</a></h6>
+													<p class="m-t-20">You can relay on our amazing features list and also our customer services.</p>
+												</div>
+											</div>
+										</div>
+									</div>
+									<!-- Column -->
+									<div class="col-md-12 m-t-20 text-center">
+										<a class="btn btn-success-gradiant btn-md btn-arrow"><span>View Feature5 code <i class="ti-arrow-right"></i></span></a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</section>
+                </div>
+            <?php include('footer.php');?>
